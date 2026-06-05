ed9d623546e837dcadbae108c56bb3e5d4641591
Orlisan <sangiovanniorlando12@gmail.com>
2026-06-05 21:23:24 +0200

    altro commit

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function scala() {
   const schermo = document.querySelector(".schermo");
   const scalaX = window.innerWidth / 1920;
   const scalaY = window.innerHeight / 1080;
-  const s = Math.max(scalaX, scalaY);
+  const s = Math.min(scalaX, scalaY);
   schermo.style.transform = `scale(${s})`;
   schermo.style.marginLeft = `${(window.innerWidth - 1920 * s) / 2}px`;
   schermo.style.marginTop = `${(window.innerHeight - 1080 * s) / 2}px`;
