ef2534ad517912fd242b7731f86dc845dbb4327b
Orlisan <sangiovanniorlando12@gmail.com>
2026-06-06 21:14:23 +0200

    altro commit

--- project-script.js
+++ project-script.js
@@ -10,13 +10,11 @@ import * as Progetti from "./var_globali.js";
   const testo = await progetto.getDescription();
   descrizione.innerHTML = testo;
   document.fonts.ready.then(() => {
-      const sfondoDescrizione = document.querySelector(".sfondo_descrizione");
-      if (sfondoDescrizione) {
-          sfondoDescrizione.style.height = (descrizione.scrollHeight + 30) + "px";
-      }
+    const sfondoDescrizione = document.querySelector(".sfondo_descrizione");
+    if (sfondoDescrizione) {
+      sfondoDescrizione.style.height = descrizione.scrollHeight + 30 + "px";
+    }
   });
-  /*const sfondoDescrizione = document.querySelector(".sfondo_descrizione");
-  sfondoDescrizione.style.height = descrizione.scrollHeight +"px";*/
   const titolo = document.querySelector(".title_project");
   titolo.textContent = progetto.getTitle();
   const bottone = document.querySelector(".download_file");
@@ -33,6 +31,23 @@ import * as Progetti from "./var_globali.js";
   imgProject.width = document.documentElement.clientWidth;
   imgProject.height = document.documentElement.clientWidth;
   document.querySelector(".schermo_sotto").appendChild(imgProject);
+  console.log(imgProject.offsetHeight < document.documentElement.clientHeight);
+  console.log(imgProject.offsetHeight);
+  console.log(document.documentElement.scrollHeight);
+  if (imgProject.offsetHeight < document.documentElement.scrollHeight) {
+    const imgProject3 = document.createElement("img");
+    imgProject3.style.position = "absolute";
+    imgProject3.style.zIndex = "0";
+    imgProject3.style.left = "0";
+    imgProject3.style.top = document.documentElement.clientWidth + "px";
+    imgProject3.src = progetto.getImgPath();
+    imgProject3.width = document.documentElement.clientWidth;
+    imgProject3.height =
+      document.documentElement.scrollHeight - imgProject.offsetHeight;
+    imgProject3.style.objectFit = "cover";
+    imgProject3.style.objectPosition = "top";
+    document.querySelector(".schermo_sotto").appendChild(imgProject3);
+  }
   const imgProject2 = document.createElement("img");
   imgProject2.style.position = "absolute";
   imgProject2.style.zIndex = "1";
